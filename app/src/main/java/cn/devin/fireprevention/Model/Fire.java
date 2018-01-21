eef763733f8b97402681e6ded7b145863ba5b35f
Devin Zhang <MeetDevin@users.noreply.github.com>
2018-01-21 22:42:22 +0800

    fire model

---
+++ app/src/main/java/cn/devin/fireprevention/Model/Fire.java
@@ -0,0 +1,25 @@
+package cn.devin.fireprevention.Model;
+
+import com.tencent.tencentmap.mapsdk.maps.model.LatLng;
+
+import java.util.List;
+
+/**
+ * Created by Devin on 2018/1/21.
+ */
+
+public class Fire {
+    List<LatLng> fireHead;
+
+    public Fire(LatLng latLng){
+        this.fireHead.add(latLng);
+    }
+
+    public void addFireHead(LatLng latLng){
+        this.fireHead.add(latLng);
+    }
+
+    public void subFireHead(int i){
+        this.fireHead.subList(i,1);
+    }
+}
